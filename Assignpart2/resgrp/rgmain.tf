104ec74398045803eee9f4e4d5b67e55ecd561fc
AmanShreeTA <aman.prasad@tigeranalytics.com>
2024-03-22 15:56:59 +0530

    Resource Group tf code added

---
+++ Assignpart2/resgrp/rgmain.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "3.95.0"
+    }
+  }
+}
+
+provider "azurerm" {
+    skip_provider_registration = true # This is only required when the User, Service Principal, or Identity running Terraform lacks the permissions to register Azure Resource Providers.
+    features {}
+  # Configuration options
+}
+
+resource "azurerm_resource_group" "resgrpTfsample" {
+  name = "MyResourceGroup"
+  location = "East US"
+}
